5a2c6c59be2e6661ae8ffdd2177840881566a7b9
Xleb <disa000ded@gmail.com>
2024-05-02 23:35:39 +0800

    ege 1234

---
+++ KASATKIN/5/18785.py
@@ -0,0 +1,10 @@
+for n in range(1000):
+    s = bin(n)[2:]
+    s = str(s)
+    if s[-1] == 0:
+        s = '1' + s + '0'
+    else:
+        s = '11' + s + '11'
+    r = int(s, 2)
+    if r > 52:
+        print(n)
