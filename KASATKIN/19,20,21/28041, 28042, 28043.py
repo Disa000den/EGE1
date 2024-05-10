f52b2fc187f5ab0c9834fefa21299ddf8020e7be
Xleb <disa000ded@gmail.com>
2024-05-10 17:31:19 +0800

    /-.-\ drochish? A ya drochy 19-21)

---
+++ KASATKIN/19,20,21/28041, 28042, 28043.py
@@ -0,0 +1,22 @@
+from functools import lru_cache
+
+
+def moves(h):
+    a = h
+    return (a + 1, a + 5, a * 3)
+
+
+@lru_cache(None)
+def game(h):
+    a = h
+    if a >= 42: return 'W'
+    if any(game(m) == 'W' for m in moves(h)): return 'P1'
+    if all(game(m) == 'P1' for m in moves(h)): return 'B1'
+    if any(game(m) == 'B1' for m in moves(h)): return 'P2'
+    if all(game(m) == 'P1' or game(m) == 'P2' for m in moves(h)): return 'B2'
+
+
+for s in range(1, 100):
+    h = s
+    if game(h) == 'B2':
+        print(s, game(h))
---
+++ KASATKIN/19,20,21/35477, 35478, 35479.py
@@ -0,0 +1,21 @@
+from functools import lru_cache
+
+
+def moves(h):
+    a, b = h
+    return (a + 1, b), (a * 2, b), (a, b + 1), (a, b * 2)
+
+@lru_cache(None)
+def game(h):
+    a, b = h
+    if a + b >= 77: return 'W'
+    if any(game(m) == 'W' for m in moves(h)): return 'P1'
+    if all(game(m) == 'P1' for m in moves(h)): return 'B1'
+    if any(game(m) == 'B1' for m in moves(h)): return 'P2'
+    if all(game(m) == 'P1' or game(m) == 'P2' for m in moves(h)): return 'B2'
+
+
+for s in range(1, 100):
+    h = 8, s
+    if game(h) == 'B1' or game(h) == 'B2':
+        print(s, game(h))
