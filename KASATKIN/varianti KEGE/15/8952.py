6d025765be9defaf560a398be97345c304240fa6
Xleb <disa000ded@gmail.com>
2024-05-12 21:18:52 +0800

    popravil 15-oe v kege tam po prioritetam ne rabotalo xd

--- KASATKIN/varianti KEGE/15/8952.py
+++ KASATKIN/varianti KEGE/15/8952.py
@@ -1,7 +1,7 @@
 def f(x, a):
-    return (x & 103 == 0) and (x & 94 != 0) <= (x & a != 0)
+    return ((x & 103 == 0) and (x & 94 != 0)) <= (x & a != 0)
 
 
 for a in range(1000):
-    if all(f(x, a) for x in range(1000)):
+    if all(f(x, a) for x in range(1, 1000)):
         print(a)
